8b02262b0d07a00c4784f7294fd2d60d14345c48
zhaoxd <1101930659@qq.com>
2020-05-28 13:58:03 +0800

    str_full2

--- python_base/if1.py
+++ python_base/if1.py
@@ -4,5 +4,15 @@ x = int(input("please enter an integer:"))
 if x <= 0:
     print("Negative changed to zero")
 else:
-    print("This is number >0")
+    print("This is number gt 0")
 
+y = int(input("Please enter an integer: "))
+
+if y <= 0:
+    print('Negative changed to zero')
+elif y == 0:
+    print('Zero')
+elif y == 1:
+    print('Single')
+else:
+    print('More')
