c2e7dd29e9153f4e404222481b61707c0b27bcb7
zhaoxd <1101930659@qq.com>
2020-05-28 13:42:22 +0800

    str_full

--- python_base/str1.py
+++ python_base/str1.py
@@ -8,3 +8,19 @@ print(word[:])
 print(word[-1])
 print(word[-1:-5])
 
+print('j' + word[0])
+
+
+#return str len
+
+print(len(word))
+
+
+#  +---+---+---+---+---+---+
+#  | P | y | t | h | o | n |
+#  +---+---+---+---+---+---+
+#  0   1   2   3   4   5   6
+# -6  -5  -4  -3  -2  -1
+
+
+
