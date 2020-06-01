e90bd32c5ee34c3c5e7e8942a68837baafc3e178
zhaoxd <1101930659@qq.com>
2020-06-01 14:12:59 +0800

    date_case

--- python_base/class1.py
+++ python_base/class1.py
@@ -59,3 +59,19 @@ print(john.name)
 
 
 #异常也是类
+class B(Exception):
+    pass
+class C(B):
+    pass
+class D(C):
+    pass
+
+for cls in [B, C, D]:
+    try:
+        raise cls()
+    except D:
+        print("D")
+    except C:
+        print("C")
+    except B:
+        print("B")
