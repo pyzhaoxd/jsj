96b2dd13d96717ed01885555bca19b71c2871986
zhaoxd <1101930659@qq.com>
2020-06-08 17:23:15 +0800

    date_case

--- python_base/vyosscript.py
+++ python_base/vyosscript.py
@@ -7,14 +7,14 @@ vyos.configure()
 try:
     vyos.set("protocols static route 203.0.113.0/25 next-hop 192.0.2.20")
     vyos.set("protocols static route 202.0.113.0/25 next-hop 192.0.2.20")
-    vyos.commit()
-    vyos.save()
-
 
 except Exception as e:
     print('this is error log',e)
 
 finally:
+    vyos.set("protocols static route 209.0.113.0/25 next-hop 192.0.2.20")
+    vyos.commit()
+    vyos.save()
     vyos.exit()
     vyos.logout()
 
