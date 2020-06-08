b5b8cd81c91b3c9a085293f7d64eccbf94b711f8
zhaoxd <1101930659@qq.com>
2020-06-08 17:20:52 +0800

    date_case

---
+++ python_base/vyosscript.py
@@ -0,0 +1,22 @@
+import vymgmt
+
+#连接设备
+vyos = vymgmt.Router('172.200.100.230', user='vyos', password='vyos', port=22)
+vyos.login()
+vyos.configure()
+try:
+    vyos.set("protocols static route 203.0.113.0/25 next-hop 192.0.2.20")
+    vyos.set("protocols static route 202.0.113.0/25 next-hop 192.0.2.20")
+    vyos.commit()
+    vyos.save()
+
+
+except Exception as e:
+    print('this is error log',e)
+
+finally:
+    vyos.exit()
+    vyos.logout()
+
+
+
